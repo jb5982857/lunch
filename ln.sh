671302aa3605a50afc4c9500778edc8f8bbc3d59
jiangbo <>
2019-01-15 20:00:05 +0800

    提交软连接和上传sh文件，更新readme

--- ln.sh
+++ ln.sh
@@ -1,6 +1,7 @@
-declare -A default_port_map=(["account"]="8080",["place"]="8081",["zuul"]="80",["eureka"]="8761")
-for key in ${!default_port_array[@]}
+declare -A default_port_map=(["account"]="8080" ["place"]="8081" ["zuul"]="80" ["eureka"]="8761")
+echo ${!default_port_map[@]}
+for key in ${!default_port_map[@]}
 do
-    ln -s /root/java/tomcat/webapp_${default_port_array[$key]}_lib/$key_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_array[$key]}/$key.jar
-    echo "ln -s /root/java/tomcat/webapp_${default_port_array[$key]}_lib/$key_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_array[$key]}/$key.jar"
+	echo "ln -s /root/java/tomcat/webapp_${default_port_map[$key]}_lib/${key}_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_map[$key]}/${key}.jar"
+	ln -s /root/java/tomcat/webapp_${default_port_map[$key]}_lib/${key}_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_map[$key]}/${key}.jar
 done
---
+++ scp.sh
@@ -0,0 +1,4 @@
+# $1 本地路径
+# $2 端口号
+# $3 jar包前缀
+scp $1 root@47.98.203.205:/root/java/tomcat/webapp_$2_lib/$3_`date +%y%m%d`.jar
