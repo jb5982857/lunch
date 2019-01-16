337bc669211e9a7e34f96d99e8c0ff119f530b41
jiangbo <>
2019-01-16 15:36:39 +0800

    可动态设置某个模块软连接，并且会软连接lib路径下最新的那一个

--- ln.sh
+++ ln.sh
@@ -1,7 +1,52 @@
+#!/bin/bash 
+
+
 declare -A default_port_map=(["account"]="8080" ["place"]="8081" ["zuul"]="80" ["eureka"]="8761")
-echo ${!default_port_map[@]}
-for key in ${!default_port_map[@]}
+echo ${!default_port_map[@]}_$#
+
+declare -A choice_map=()
+
+if [ $# = 0 ]; then
+
+	for key in ${!default_port_map[@]}
+	do
+		choice_map[$key]=${default_port_map[$key]}		
+	done
+
+else
+	for i in $@
+	do
+        	echo "i:"$i
+        	choice_map[$i]=${default_port_map[$i]}
+	done
+fi #ifend
+
+echo ${!choice_map[@]}_${choice_map[@]}
+
+#$1 文件路径 
+function newestFile() 
+{     
+        echo $(ls $1 | sort -r)
+}   
+
+for key in ${!choice_map[@]}
 do
-	echo "ln -s /root/java/tomcat/webapp_${default_port_map[$key]}_lib/${key}_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_map[$key]}/${key}.jar"
-	ln -s /root/java/tomcat/webapp_${default_port_map[$key]}_lib/${key}_`date +%y%m%d`.jar /root/java/tomcat/webapp_${default_port_map[$key]}/${key}.jar
+
+	file="/root/java/tomcat/webapp_${choice_map[$key]}/${key}.jar"
+	if [[ -f "$file" ]]; then
+		echo "file $file  exist ,del now!"
+		rm -rf /root/java/tomcat/webapp_${choice_map[$key]}/${key}.jar	
+	fi #ifend
+
+	files=($(newestFile "/root/java/tomcat/webapp_${choice_map[$key]}_lib/"))
+	echo "files "$files
+
+	if [ $files ];then
+		echo "ln -s /root/java/tomcat/webapp_${choice_map[$key]}_lib/${files[0]} /root/java/tomcat/webapp_${choice_map[$key]}/${key}.jar"
+
+        ln -s /root/java/tomcat/webapp_${choice_map[$key]}_lib/${files[0]} /root/java/tomcat/webapp_${choice_map[$key]}/${key}.jar
+	else 
+		echo "it does not find any file in /root/java/tomcat/webapp_${choice_map[$key]}_lib/"
+	fi	
+
 done
